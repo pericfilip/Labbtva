d2b95b9ed393f7cf6335d064efb874ef57cd9539
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-20 20:49:12 +0200

    code cleanUp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,41 +8,42 @@ public class Main {
     public static void main(String[] args) {
         File startingFolder = new File("./");
         Scanner anv = new Scanner(System.in);
-
         String letastring;
+
         System.out.println("Hallå där! ");
         System.out.print("Vänligen ange sökord: ");
         letastring = anv.next();
         System.out.println("----------");
+
         try {
             printInfo(startingFolder, letastring);
         } catch (Exception e) {
             System.out.println("Något har blivit fel");
             e.printStackTrace();
         }
-
     }
+
     public static void printInfo(File file, String string) {
-        try{
-        if (file.isDirectory()) {
-            File[] folderCon = file.listFiles();
-            for(File convag : folderCon){
-                printInfo(convag, string);
-            }
-        }    else if (file.isFile()){
-            Scanner fileScan = new Scanner(file);
+        try {
+            if (file.isDirectory()) {
+                File[] folderCon = file.listFiles();
+                for(File convag : folderCon) {
+                    printInfo(convag, string);
+                }
+            } else if (file.isFile()) {
+                Scanner fileScan = new Scanner(file);
 
-            while(fileScan.hasNextLine()) {
-                String fileSC = fileScan.nextLine();
-                String filescanLow = fileSC.toLowerCase();
+                while(fileScan.hasNextLine()) {
+                    String fileSC = fileScan.nextLine();
+                    String filescanLow = fileSC.toLowerCase();
 
-                if (filescanLow.contains(string.toLowerCase())) {
-                    System.out.println("Funnen i följande: " + file.getCanonicalPath());
+                    if (filescanLow.contains(string.toLowerCase())) {
+                        System.out.println("Funnen i följande: " + file.getCanonicalPath());
+                    }
                     return;
                 }
+                fileScan.close();
             }
-            fileScan.close();
-        }
             if(!file.canRead()) {
                 System.err.println("ERROR: Kan inte läsa " + file.getCanonicalPath());
             }
@@ -50,4 +51,5 @@ public class Main {
             e.printStackTrace();
         }
     }
+    
 }
